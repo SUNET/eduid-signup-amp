5afc38c2b4013cd0ca5f6a13707d3c77a0b9b105
Fredrik Thulin <fredrik@thulin.net>
2014-03-31 08:34:28 +0200

    version 0.2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
-version = '0.2.0'
+version = '0.2.1'
 
 requires = [
     'pymongo==2.6.3',
