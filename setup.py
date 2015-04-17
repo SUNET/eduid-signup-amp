5570950e7baa0406ac26ff6d1b70ce7d6f223bf4
Johan Lundberg <lundberg@nordu.net>
2015-04-17 10:48:37 +0200

    Bumped eduid-am version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ version = '0.2.8'
 
 requires = [
     'pymongo>=2.8,<3',   # CI fails to build unless a version (same as in eduid_am) is required here :(
-    'eduid_am>=0.5.2',
+    'eduid_am>=0.5.3',
 ]
 
 testing_extras = [
